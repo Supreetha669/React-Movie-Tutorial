2623dbf712b0168c3219775cb03841fc1aeabb8e
Supreetha669 <supreethahm1922@gmail.com>
2025-10-23 20:22:59 +0530

    Initial commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "0d6db90febade2b01ba6a44c5cf4d343";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
